ebcfafe8db8eafd76b071025169de8d76f87aeec
emmyxth <emmyst@stanford.edu>
2023-07-15 14:15:40 -0700

    profile card work in progress

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,23 +1,53 @@
 import { Box } from "@mui/material";
+import { Card, CardMedia } from "@material-ui/core/CardMedia";
+import { styled } from '@mui/material/styles';
+
 
 const ProfileCard = ({text}) => {
     return (
         <>
-            <Box
-                sx={{
-                    width: 303,
-                    height: 395,
-                    backgroundColor: 'F8F9FC',
-                    border: "1px solid black",
-                    borderRadius: 3,
-                    '&:hover': {
-                        backgroundColor: 'black'
-                    }
-                }}
-                >
-            </Box>
+            <Card sx={{
+                        width: 303,
+                        height: 395,
+                        backgroundColor: 'F8F9FC',
+                        border: "1px solid black",
+                        borderRadius: 3,
+                        '&:hover': {
+                            backgroundColor: 'black'
+                        }
+                    }}
+            >
+                <CardMedia 
+                    // sx={{width:303}}
+                    // <CardMedia
+                    sx={{ height: 140 }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="green iguana"
+                  />
+            </Card>
         </>
     );
 };
+
+
+// const ProfileCard = ({text}) => {
+//     return (
+//         <>
+//             <Box
+//                 sx={{
+//                     width: 303,
+//                     height: 395,
+//                     backgroundColor: 'F8F9FC',
+//                     border: "1px solid black",
+//                     borderRadius: 3,
+//                     '&:hover': {
+//                         backgroundColor: 'black'
+//                     }
+//                 }}
+//                 >
+//             </Box>
+//         </>
+//     );
+// };
 
 export default ProfileCard;
