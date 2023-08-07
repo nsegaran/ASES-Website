bb867074decfbd97f40fb8727cb1c65dead8cccb
nsegaran <104466978+nsegaran@users.noreply.github.com>
2023-08-06 20:59:51 -0700

    fixed that one weird bug

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import LongButtonWArrowCustomized from "./components/LongButtonWithArrow";
 import LongButtonWOArrowCustomized from "./components/LongButtonWOArrow";
 import ShortButtonCustomized from "./components/ShortButton";
-import ProfileCard from "./components/ProfileCard";
+import TopBar from "./components/TopBar";
+import Sponsors from "./components/SponsorsBanner.js";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import FAQSection from "./pages/Home/sections/FAQs";
 import Timeline from "./pages/Home/sections/Timeline";
+import Testimonials from "./pages/Home/sections/Testimonials";
 import profileData from "./components/textContent/homeProfileCards.json";
 import { motion } from "framer-motion";
 
@@ -18,27 +21,13 @@ const theme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} sx={{display: "flex", flexDirection: "column", height: "100vh"}}>
+      <TopBar/>
+      <Testimonials/>
       <Timeline />
-
       <FAQSection />
-      {/* <div className="App min-vh-100 d-flex justify-content-center align-items-center">
-        <LongButtonWArrowCustomized />
-        <LongButtonWOArrowCustomized />
-        <ShortButtonCustomized />
-      </div> */}
-      <div className="d-flex flex-lg-row flex-column justify-content-center align-items-center space-x-6 space-y-6">
-        {profileData.map((m) => {
-          return (
-            <ProfileCard
-              name={m.name}
-              role={m.role}
-              hoveredText={m.description}
-              image={m.image}
-            />
-          );
-        })}
-      </div>
+      <Sponsors />
+      <Footer/>
     </ThemeProvider>
   );
 }
